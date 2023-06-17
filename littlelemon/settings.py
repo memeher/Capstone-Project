4ef85d77bdb89667e6abab498282ff2391a332c5
memeher <hoomaninvisible@gmail.com>
2023-06-17 12:39:36 +0530

    changed version

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "LittleLemon",
         'USER': 'root',
-        'PASSWORD': "Meher9948281785@sweety",
+        'PASSWORD': "",
         "HOST":"127.0.0.1",
         "PORT": "3306",
         "OPTIONS":{
